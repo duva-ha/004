41f7ca98079bd0d6a76a3a2eca02b2e38c5b8708
duva-ha <dvhai.vi@gmail.com>
2026-05-09 23:47:27 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import EnglishBot from './components/EnglishBot';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+      <EnglishBot />
+    </div>
+  );
+}
+export default App;
